670bc6d7fb9dca42f9866b9381f5914284a718fe
Skolas3654 <a3654058@gmail.com>
2022-01-19 12:48:56 -0500

    -Rounding issue with coffee(), outputed values are sometimes 0.01 off

--- assignments/hw1/hw1.py
+++ assignments/hw1/hw1.py
@@ -2,7 +2,8 @@
 Name: <your name goes here – first and last>
 <ProgramName>.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: <Brief, one or two sentence description of the
+ problem that this program solves, in your own words.>
 
 Certification of Authenticity:
 <include one of the following>
@@ -12,23 +13,37 @@ I certify that this assignment is my own work, but I discussed it with: <Name(s)
 
 
 def calc_rec_area():
-    pass
+    width = eval(input("Enter the width:"))
+    length = eval(input("Enter the length:"))
+    area = width*length
+    print("The area of the rectangle is:", area)
 
 
 def calc_volume():
-    pass
+    length = eval(input("Enter the length:"))
+    width = eval(input("Enter the width:"))
+    height = eval(input("Enter the height:"))
+    volume = length*width*height
+    print("The volume is", volume)
 
 
 def shooting_percentage():
-    pass
+    total_shots = eval(input("Enter the player's total shots: "))
+    player_shots = eval(input("Enter the shots the player made:"))
+    shot_percent = (player_shots/total_shots)*100
+    print("Your total is:",shot_percent, "%")
 
 
 def coffee():
-    pass
+    coffee_pounds = eval(input("How many pounds of coffee would you like? "))
+    total = coffee_pounds*(10.50+0.86) + 1.5
+    print("Your total is $", total)
 
 
 def kilometers_to_miles():
-    pass
+    kilometers = eval(input("How many kilometers did you travel? "))
+    miles = kilometers/1.61
+    print("You traveled", miles, "miles")
 
 
 if __name__ == '__main__':
