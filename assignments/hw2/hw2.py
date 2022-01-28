0823d13349393be0537ce5a6ed7152608c728f24
Skolas3654 <a3654058@gmail.com>
2022-01-28 18:03:07 -0500

    Pushing h.w. 2
    test 2 did not work on my end so I couldnt grade myself

--- assignments/hw2/hw2.py
+++ assignments/hw2/hw2.py
@@ -13,23 +13,59 @@ import math
 
 
 def sum_of_threes():
-    pass
+    upper_bond = eval(input("What is the upper bond: \n"))
+    for i in range(0, upper_bond + 1, 3):
+        print(i)
+    result = upper_bond * 3
+    print("Sum of threes is", result)
+
 
 
 def multiplication_table():
-    pass
+    index = 1
+    min_row = 1
+    max_row = 11
+    for i in range(1,11):
+        for y in range(min_row,max_row,index):
+
+            print(y, end=" ")
+        print("\n")
+        index = index + 1
+        max_row = max_row + 10
+        min_row = min_row + 1
+
 
 
 def triangle_area():
-    pass
+    a = eval(input("What length is side a:\n"))
+    b = eval(input("What length is side b:\n"))
+    c = eval(input("What length is side c:\n"))
+    area = (a+b+c)/2
+    print("The area is",area)
+
 
 
 def sum_squares():
-    pass
+    lower_range = eval(input("Enter the lower range:\n"))
+    upper_range = eval(input("Enter the upper range:\n"))
+    sum = 0
+    for i in range(lower_range,upper_range+1):
+        sum = sum + i * i
+    print("Your result is",sum)
+
+
+
+
 
 
 def power():
-    pass
+    base = eval(input("Enter base: \n"))
+    static_base = base
+    exponent = eval(input("Enter exponent: \n"))
+    for i in range(exponent-1):
+        base = (base * static_base)
+    print(static_base,"^",exponent,"=",base)
+
 
 
 if __name__ == '__main__':
